093a9c8a60c729fab1c4d60b5b0b513a108ec66f
nuyemlc <nuyemlc@gmail.com>
2025-05-01 02:48:34 +0100

    Installed mysql2 for better performance to connect to mysql database & started intial get api route for my world map page

--- Backend/server.js
+++ Backend/server.js
@@ -18,6 +18,18 @@ app.get('/foods', async (req, res) => {
   }
 });
 
+app.get('/api/origins', async (req, res) => {
+    try {
+      const [rows] = await db.query(
+        "SELECT name, origin_country FROM foods WHERE origin_country IS NOT NULL"
+      );
+      res.json(rows);
+    } catch (err) {
+      console.error('Error fetching origins:', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });  
+
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
